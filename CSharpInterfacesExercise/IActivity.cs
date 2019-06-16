0cd08a22d1099cd48cc29c32b8a14515be81e583
mroyski <mroyski@gmail.com>
2019-06-16 14:54:18 -0400

    IActivity interface

--- CSharpInterfacesExercise/IActivity.cs
+++ CSharpInterfacesExercise/IActivity.cs
@@ -6,11 +6,8 @@ using System.Threading.Tasks;
 
 namespace CSharpInterfacesExercise
 {
-    class Program
+    public interface IActivity
     {
-        static void Main(string[] args)
-        {
-
-        }
+        void Execute();
     }
 }
