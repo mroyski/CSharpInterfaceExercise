2cc1f0c1cadb3cd8c2ce5c6eedba524c61c341ff
mroyski <mroyski@gmail.com>
2019-06-16 15:00:27 -0400

    workflow uses list of activities

---
+++ CSharpInterfacesExercise/WorkFlow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpInterfacesExercise
+{
+    public class WorkFlow
+    {
+        private readonly IList<IActivity> _activities;
+
+        public WorkFlow()
+        {
+            _activities = new List<IActivity>();
+        }
+    }
+}
---
+++ CSharpInterfacesExercise/WorkFlowEngine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpInterfacesExercise
+{
+    public class WorkFlowEngine
+    {
+        public void Run(WorkFlow workFlow)
+        {
+
+        }
+    }
+}
